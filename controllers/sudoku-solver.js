1d9bb7431afa0f52f57b5e2c317403d4032211d9
Stanislav <stanislav.pogorskii@gmail.com>
2023-10-28 16:59:39 +0400

    feature complete

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,110 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    const result = puzzleString.search(/[^\d.]/gi);
+    if (result !== -1) return -1;
+    if (puzzleString.length !== 81) return 0;
+    return 1;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    if (puzzleString[row].includes(value)) return false;
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    const columnString = puzzleString.map((row) => row[column]);
+    if (columnString.includes(value)) return false;
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    // Check if the cell is empty.
+    if (puzzleString[row * 9 + column] !== ".") {
+      return false;
+    }
 
+    // Get the start index of the square.
+    const squareRow = Math.floor(row / 3);
+    const squareColumn = Math.floor(column / 3);
+    const squareStartIndex = squareRow * 3 * 9 + squareColumn * 3;
+
+    // Check if the value is already present in the square.
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (puzzleString[squareStartIndex + i * 9 + j] === value) {
+          return false;
+        }
+      }
+    }
+
+    // If the value is not already present in the square, return true.
+    return true;
   }
 
   solve(puzzleString) {
-    
+    const setBits = (board, rows, columns, subgrids) => {
+      for (let row = 0; row < 9; row++) {
+        for (let column = 0; column < 9; column++) {
+          if (board[row][column] != ".") {
+            let bitMask = 1 << (parseInt(board[row][column]) - 1);
+            let subgrid = Math.floor(row / 3) * 3 + Math.floor(column / 3);
+
+            rows[row] |= bitMask;
+            columns[column] |= bitMask;
+            subgrids[subgrid] |= bitMask;
+          }
+        }
+      }
+    };
+
+    const backtrack = (board, index, rows, columns, subgrids) => {
+      if (index > 80) return true;
+
+      let row = Math.floor(index / 9);
+      let column = Math.floor(index % 9);
+
+      if (board[row][column] != ".")
+        return backtrack(board, index + 1, rows, columns, subgrids);
+
+      let subgrid = Math.floor(row / 3) * 3 + Math.floor(column / 3);
+
+      for (let i = 0; i < 9; ++i) {
+        let mask = 1 << i;
+        if (
+          rows[row] & mask ||
+          columns[column] & mask ||
+          subgrids[subgrid] & mask
+        )
+          continue;
+
+        board[row][column] = (i + 1).toString();
+        rows[row] |= mask;
+        columns[column] |= mask;
+        subgrids[subgrid] |= mask;
+
+        if (backtrack(board, index + 1, rows, columns, subgrids)) return true;
+
+        board[row][column] = ".";
+        rows[row] ^= mask;
+        columns[column] ^= mask;
+        subgrids[subgrid] ^= mask;
+      }
+
+      return false;
+    };
+
+    const solveSudoku = (board) => {
+      const rows = Array(9).fill(0),
+        columns = Array(9).fill(0),
+        subgrids = Array(9).fill(0);
+
+      setBits(board, rows, columns, subgrids);
+
+      backtrack(board, 0, rows, columns, subgrids);
+    };
+
+    solveSudoku(puzzleString);
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,65 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+  const solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
+    if (!puzzle || !coordinate || !value)
+      return res.json({ error: "Required field(s) missing" });
+    const validity = solver.validate(puzzle);
+    if (validity === -1)
+      return res.json({ error: "Invalid characters in puzzle" });
+    if (validity === 0)
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    const coordRegex = /^[A-I][1-9]$/;
+    if (!coordRegex.test(coordinate))
+      return res.json({ error: "Invalid coordinate" });
 
-    });
+    const numRegex = /^[1-9]$/;
+    if (!numRegex.test(value)) return res.json({ error: "Invalid value" });
+
+    const board = puzzle.match(/.{1,9}/g) ?? [];
+    const rowsMap = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8 };
+    const row = rowsMap[coordinate[0].toUpperCase()];
+    const column = coordinate[1] - 1;
+
+    if (board[row][column] === value) return res.json({ valid: true });
+
+    const conflictsArr = [];
+    if (!solver.checkRowPlacement(board, row, column, value))
+      conflictsArr.push("row");
+    if (!solver.checkColPlacement(board, row, column, value))
+      conflictsArr.push("column");
+    if (!solver.checkRegionPlacement(puzzle, row, column, value))
+      conflictsArr.push("region");
+
+    if (conflictsArr.length > 0)
+      return res.json({ valid: false, conflict: conflictsArr });
+    return res.json({ valid: true });
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const puzzle = req.body.puzzle;
+    if (!puzzle) return res.json({ error: "Required field missing" });
+    const validity = solver.validate(puzzle);
+    if (validity === -1)
+      return res.json({ error: "Invalid characters in puzzle" });
+    if (validity === 0)
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+
+    let board = puzzle.match(/.{1,9}/g) ?? [];
+
+    const newBoard = board.map((row) => row.split(""));
+
+    solver.solve(newBoard);
+
+    const solution = newBoard.flat().join("");
+    if (solution.includes("."))
+      return res.json({ error: "Puzzle cannot be solved" });
+    res.json({ solution });
+  });
 };
